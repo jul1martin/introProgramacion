d3a95ea0b03972f7d52d3a6ee0931d024d84a9ff
jul1martin <87287562+jul1martin@users.noreply.github.com>
2023-09-11 00:21:31 -0300

    Practica 4

--- guias/4/practica4.hs
+++ guias/4/practica4.hs
@@ -1,103 +1,159 @@
 -- fibonacci
 
-fibonacci :: Int -> Int
+fibonacci :: Integer -> Integer
 fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci n = (fibonacci(n - 1)) +
               (fibonacci(n - 2))
 
-parteEntera :: Float -> Int
+parteEntera :: Float -> Integer
 parteEntera x | 0 <= x && x < 1 = 0
               | x > -1 && x <= 0 = -1
               | x >= 1 = 1 + parteEntera(x - 1) 
               |otherwise = (-1) + parteEntera(x + 1) 
 
 -- Ejercicio 4
--- problema sumaImpares (Int) : Int {
+-- problema sumaImpares (Integer) : Integer {
 --       requiere: { El valor ingresado tiene que ser estrictamente natural }
 --       asegura: { Devuelve 0 si el valor ingresado es 0 }
 --       asegura: { Sumara recursivamente numeros impares hasta llegar a la n cantidad de sumas }
 -- }
 
-sumaImpares :: Int -> Int 
-sumaImpares times | times == 1 = 1
-                  |  otherwise = 1 +  (2 * (times - 1)) + sumaImpares (times - 1)  
+-- sumaImpares :: Integer -> Integer 
+-- sumaImpares times | times == 1 = 1
+--                   |  otherwise = 1 +  (2 * (times - 1)) + sumaImpares (times - 1)  
+
+sumaImpares :: Integer -> Integer 
+sumaImpares n | n == 0 = 0
+                  |  otherwise = ((n*2) - 1) + sumaImpares (n - 1)  
+
+
+
 
 esPar x = mod x 2 == 0 
 
 --Ejercicio 5
-medioFact :: Int-> Int
+medioFact :: Integer-> Integer
 medioFact n | n == 0 || n < 0 = 1
             | otherwise = n * medioFact(n - 2)
 
 --Ejercicio 6
--- problema sumaDigitos (Int): Int {
+-- problema sumaDigitos (Integer): Integer {
 --       requiere: { Tiene que ser un numero natural }
 --       asegura: { result es el mismo valor que se ingreso si los digitos es igual a 1 }
 --       asegura: { Sumara digito por digito del valor ingresado }
 -- }
 
-sumaDigitos :: Int -> Int
+sumaDigitos :: Integer -> Integer
 sumaDigitos x | x < 10 = x
               | otherwise = (mod x 10) + sumaDigitos (div x 10)
 
 -- Ejercicio 7
 
-todosDigitosIguales :: Int -> Bool
+todosDigitosIguales :: Integer -> Bool
 todosDigitosIguales n | n < 10 = True
                       | otherwise = ultimoDigito n == ultimoDigito (sacarUltimo n) && 
                                     todosDigitosIguales(sacarUltimo n)
 
-ultimoDigito :: Int -> Int
+ultimoDigito :: Integer -> Integer
 ultimoDigito n = mod n 10
 
-sacarUltimo :: Int -> Int
+sacarUltimo :: Integer -> Integer
 sacarUltimo n = div n 10
 
 --Ejercicio 8
 -- la hice asi, sin ser recursiva
 
-iesimoDigito :: Int -> Int -> Int
+iesimoDigito :: Integer -> Integer -> Integer
 iesimoDigito n i | cantDigitos n == i = mod n 10
                  | otherwise = mod (div n (10^(cantDigitos (n) - i))) 10
 
 --recursivamente
 
-iesimoDigitoRec :: Int -> Int -> Int
+iesimoDigitoRec :: Integer -> Integer -> Integer
 iesimoDigitoRec n i | cantDigitos n == i = ultimoDigito n
                     | otherwise = iesimoDigitoRec (sacarUltimo n) i 
-                    where ultimoDigito n = mod n 10
-                          sacarUltimo n = div n 10
 -- ultimoDigito (div n (10^(cantDigitos (n) - i)))
 
-cantDigitos :: Int -> Int
+cantDigitos :: Integer -> Integer
 cantDigitos n | n < 10 = 1
               | otherwise = 1 + cantDigitos (sacarUltimo n)
-              where sacarUltimo n = div n 10
 
 --Ejercicio 9
--- problema esCapicua (Int): Bool {
+-- problema esCapicua (Integer): Bool {
 --       require: { Tiene que ingresar un numero natural estrictamente mayor a 0 }
 --       asegura: { Revisa digito por digito, en el que result = True si todos los digitos son iguales }
 --       asegura: { result = false, si al menos un digito es distinto a otro}
 -- }
 
-esCapicua :: Int -> Bool
+esCapicua :: Integer -> Bool
 esCapicua x | cantDigitos x == 1 = True
             | iesimoDigitoRec x ((cantDigitos x) - 1) == (ultimoDigito x) = esCapicua (sacarUltimo x)
             | otherwise = False
 
 --Ejercicio 11
--- problema eAprox (Int): Float {
+-- problema eAprox (Integer): Float {
 --       require: { True }
 --       asegura: { retorna = 1 si el valor es 1  }
 --       asegura: { retorna el valor mas aproximado a e dependiendo el valor ingresado }
 -- }
 
-eAprox :: Int -> Float
+eAprox :: Integer -> Float
 eAprox x | x == 0 = 1
-         | otherwise = (1 / (factorial x)) + eAprox (x - 1)
+         | otherwise = (1 / (factorial (fromIntegral x))) + eAprox (x - 1)
          
+
 factorial :: Float -> Float
 factorial x | x == 1 = 1
             | otherwise = x * factorial (x - 1)
+
+-- Ej 12 problema raizDe2Aprox (Integer n): Float {
+--       requiere: { Tiene que ser un numero natural  y n > 1 }
+--       asegura: { 
+--             n = 1 -> result = 2
+--             n > 1 -> result sera el resultado de una sucesión 
+--       }
+-- }
+
+raizDe2Aprox :: Integer -> Float
+raizDe2Aprox 1 = 2
+raizDe2Aprox n = 2 + (1 / raizDe2Aprox (n-1)) - 1 
+
+-- Ej 13 problema dobleSumatoria (n : Integer, m: Integer): Integer {
+--       requiere: { True }
+--       asegura: {
+--             Si n = 1, result = 1
+--             Si m = 1, result = Sumatoria n
+--             Sino realiza las 2 sumatorias
+--       }
+-- }
+
+sumatoria :: (Integer, Integer) -> Integer
+sumatoria (n,i) | i == 1 = n
+                | otherwise = n + sumatoria (n,i - 1) 
+
+dobleSumatoria :: Integer -> Integer -> Integer
+dobleSumatoria n m | 1 == n = sumatoria (1, m) 
+                   | otherwise = n^m + (dobleSumatoria (n-1) m)
+
+-- Ejercicio 14
+
+sumaPotencias :: Integer -> Integer -> Integer -> Integer
+sumaPotencias q n m |  n == 1 || m == 1 = q^(n + m)
+                    | otherwise = q^(n + m) + (sumaPotencias q (n-1) (m-1))
+
+--Ej 17
+-- problema esFibonacci (n: Z) : B {
+--    requiere: { n ≥ 0 }
+--    asegura: { resultado = true ↔ n es alg´un valor de la secuencia de Fibonacci definida en el ejercicio 1}
+-- }
+
+
+
+existsInFibonacci :: Integer -> Integer -> Bool
+existsInFibonacci n fibo | fibo == 0 && n /= 0 = False
+                         | otherwise = n == (fibonacci fibo) || (existsInFibonacci n (fibo-1))
+
+esFibonacci :: Integer -> Bool
+esFibonacci n | existsInFibonacci n n == True = True
+              | otherwise = False
