38a86642f350bcea4d2f360f035d94b21d6ce488
jul1martin <julianmartiniscoff@gmail.com>
2023-09-16 18:12:20 -0300

    Ej 5

--- guias/prac5.hs
+++ guias/prac5.hs
@@ -1,3 +1,4 @@
+-- import Test.HUnit
 -- TRATAR DE HACERLO CON PATTERN MATCHING
 
 --Ejercicio 1.1
@@ -6,15 +7,63 @@ longitud :: [t] -> Integer
 longitud [] = 0
 longitud (x:xs) = 1 + longitud xs
 
+ultimo :: (Eq t) => [t] -> t
+ultimo [x] = x
+ultimo (x:xs) = ultimo xs
+
+principio :: (Eq t) => [t] -> t
+principio (x:xs) = x
+
+reverso :: (Eq t) => [t] -> [t]
+reverso [x] = [x]
+reverso (x:xs) = (reverso xs)++[x]   
 
 -- EJercicio 2.4
 pertenece :: (Eq t) => t -> [t] -> Bool
 pertenece _ [] = False
 pertenece e (x:xs) = x == e || pertenece e xs
 
+noPertenece :: (Eq t) => t -> [t] -> Bool
+noPertenece _ [] = True
+noPertenece e (x:xs) = x /= e && noPertenece e xs
+
+todosIguales :: (Eq t) => [t] -> Bool
+todosIguales [x] = True
+todosIguales (x:xs) = pertenece x xs && (todosIguales xs)
+
+todosDistintos :: (Eq t) => [t] -> Bool
+todosDistintos [x] = True
+todosDistintos (x:xs) = noPertenece x xs && (todosDistintos xs)
+
 hayRepetidos :: (Eq t) => [t] -> Bool
 hayRepetidos [] = False
 hayRepetidos (x:xs) = hayRepetidos xs || pertenece x xs
+
+quitar :: Integer -> [Integer] -> [Integer]
+quitar _ [] = []
+quitar n (x:xs) | n /= x = (x) : (quitar n xs)
+                | otherwise = xs
+
+quitarTodos :: Integer -> [Integer] -> [Integer]
+quitarTodos _ [] = []
+quitarTodos n (x:xs) | n /= x = (x) : (quitarTodos n xs)
+                | otherwise = quitarTodos n xs
+
+quitarTodosEq :: (Eq t) => t -> [t] -> [t]
+quitarTodosEq _ [] = []
+quitarTodosEq n (x:xs) | n /= x = (x) : (quitarTodosEq n xs)
+                | otherwise = quitarTodosEq n xs
+
+eliminarRepetidos :: (Eq t) => [t] -> [t]
+eliminarRepetidos [] = []
+eliminarRepetidos xs     | todosDistintos xs = xs
+eliminarRepetidos (x:xs) | otherwise = x:(eliminarRepetidos (quitarTodosEq x xs))
+
+mismosElementos :: (Eq t) => [t] -> [t] -> Bool
+mismosElementos [] [] = True 
+mismosElementos _ [] = False 
+mismosElementos [] _ = False
+mismosElementos (x:xs) (y:ys) = ((pertenece x ys && pertenece y xs) || x == y) && mismosElementos (quitarTodosEq y xs) (quitarTodosEq x ys) 
 
 -- Ejercicio 3.1
 
@@ -55,8 +104,8 @@ ordenar [] = []
 --                | otherwise = (minimo xs):(ordenar xs)
 ordenar xs = (minimo xs) : ordenar (quitar (minimo xs) xs)
 
-quitar :: Integer -> [Integer] -> [Integer]
-quitar _ [] = []
-quitar n (x:xs) | n /= x = (x) : (quitar n xs)
-                | otherwise = xs
+-- testSuiteMultiplosDeN = test [
+--      "lista vacia" ~: (multiplosDeN 4 []) ~?= [],
+--      "valor 0,mult 1" ~: (multiplosDeN 0 []) ~?= []
+-- ]
 
