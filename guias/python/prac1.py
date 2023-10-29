2c6ebbd222ef7d683baa49df785f23adad39f505
jul1martin <julianmartiniscoff@gmail.com>
2023-10-29 03:34:40 -0300

    ejercicios

--- guias/python/prac1.py
+++ guias/python/prac1.py
@@ -28,3 +28,37 @@ farenheit: float = 100
 
 def farenheitCelsius (temp: float):
      return print(((temp - 32) * 5) / 9)
+
+estrofa: str = "Mentira votala \n  haciendo willy daioafioasifoasidoasio \n peron"
+
+def imprimirDosVeces(texto: str) -> str:
+     return texto * 2
+
+
+def esMultiploDe (n: int, m: int) -> bool:
+     return n % m == 0
+
+def esPar(num: int) -> bool:
+     return esMultiploDe(num, 2)
+
+def cantidadDePizzas(comensales: int, minCant: int) -> int:
+     cantPorciones: int = comensales * minCant
+
+     return math.ceil(cantPorciones / 8)
+
+# Ejercicio 3
+
+def algunoEs0(n1: int, n2: int) -> bool:
+     return n1 == 0 or n2 == 0
+
+def ambosSon0(n1: int, n2: int) -> bool:
+     return n1 == 0 and n2 == 0
+
+def esNombreLargo(nombre: str) -> bool:
+     return len(nombre) >= 3 and len(nombre) <= 8
+
+def esBisiesto(anio: int) -> bool:
+     return (anio % 400 == 0) or (anio % 4 == 0 and anio % 100 != 0)
+
+print(esBisiesto(2100))
+# print(esNombreLargo(1))
